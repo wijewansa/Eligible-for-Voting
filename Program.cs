c0c00dae30bdbf2f77ebfeff9b9a1afc40885b96
Piumi <56905494+KalpaniPiumi@users.noreply.github.com>
2020-10-03 15:42:24 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vote
+{
+    class Program
+    {
+        private static string name;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name : ");
+                name = Console.ReadLine();
+            Console.WriteLine("Enter your age :");
+            Float age=int.Parse(Console.ReadLine());
+            if(age>=18)
+            {
+                Console.WriteLine(name+", you are eligible for voting");
+            }
+            else
+            {
+                Console.WriteLine("Sorry "+name+"\nyou are not eligible for voting");
+            }
+            Console.ReadKey();
+        }
+    }
+}
